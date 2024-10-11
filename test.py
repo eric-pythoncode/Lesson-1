9844fd36c226f1a5b469486db8d6894ec5c8995d
eric-pythoncode <endlesshourgames@gmail.com>
2024-10-10 20:20:20 -0700

    hummels

---
+++ test.py
@@ -0,0 +1,30 @@
+import math
+
+x = min(4, 5, 6, 7, 8, 9, 10, 99, 8247, 22)
+y = max(4, 5, 6, 7, 8, 9, 10, 99, 8247, 22)
+
+print(x)
+print(y)
+
+num = abs(-4.55)
+
+print(num)
+
+a = pow(4, 3)
+print(pow)
+
+sqms = math.sqrt(64)
+print(sqms)
+
+b = math.ceil(1.4)
+c = math.ceil(1.4)
+
+print(b)
+print(c)
+
+print(math.pi)
+
+n1 = 48
+n2 = 18000
+
+print(math.gcd(n1,n2))
