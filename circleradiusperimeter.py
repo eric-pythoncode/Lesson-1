26653b08d98ff16fd0c5daa29e6f7faca624e33e
eric-pythoncode <endlesshourgames@gmail.com>
2024-11-14 20:35:44 -0800

    bvb

---
+++ circleradiusperimeter.py
@@ -0,0 +1,18 @@
+import math
+
+class circle:
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return math.pi * (self.radius ** 2)
+    
+    def perimeter(self):
+        return math.pi * 2 * self.radius
+    
+
+radius = int(input("Enter the radius of the cirle:"))
+c1 = circle(radius)
+
+print(f"The radius of the circle is: {c1.area():.2f}")
+print(f"The perimeter of the circle is: {c1.perimeter():.2f}")
--- polymorphism.py
+++ polymorphism.py
@@ -100,26 +100,3 @@ makepayment(creditcard, 100)
 makepayment(paypal, 100)
 makepayment(banktransfer, 100)
 
-
-#homework----------------------------------------------------------------------
-
-
-import math
-
-class circle:
-    def __init__(self, radius):
-        self.radius = radius
-
-    def area(self):
-        return math.pi * (self.radius ** 2)
-    
-    def perimeter(self):
-        return math.pi * 2 * self.radius
-    
-
-radius = int(input("Enter the radius of the cirle:"))
-c1 = circle(radius)
-
-print(f"The radius of the circle is: {c1.area():.2f}")
-print(f"The perimeter of the circle is: {c1.perimeter():.2f}")
-
